c49d42ced993a83e3a3e9f6f209e86ac3aec984f
nicodalessandro11 <nicodalessandro11@gmail.com>
2023-06-21 03:03:41 +0200

    cfu s010 finished

---
+++ s010/s010-cfu-06/s010-cfu-06-vue-project/src/components/Form.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from 'vue';
+const inputValue = ref("");
+
+watch(inputValue, (oldValue, newValue) => {
+    console.log(newValue);
+})
+
+const reset = () => inputValue.value = "";
+</script>
+
+<template>
+    <input type="text" v-model.lazy="inputValue">
+    <button @click="reset">Reset</button>
+</template>
+
+<style scoped></style>
