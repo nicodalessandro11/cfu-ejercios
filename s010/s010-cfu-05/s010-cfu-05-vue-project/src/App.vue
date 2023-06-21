1185defaa73795e33f9de61dbeb1196691b2acea
nicodalessandro11 <nicodalessandro11@gmail.com>
2023-06-21 02:25:26 +0200

    changes cfu s010 05

--- s010/s010-cfu-05/s010-cfu-05-vue-project/src/App.vue
+++ s010/s010-cfu-05/s010-cfu-05-vue-project/src/App.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from 'vue';
 
-<template></template>
+const name = ref("Nico")
+const surname = ref("D'Alessandro")
+
+const fullName = computed(() => name.value + ' ' + surname.value)
+
+const changeName = (parameter) => name.value = parameter
+
+
+</script>
+
+<template>
+    <h2 v-text="fullName"></h2>
+    <input type="text" v-model="newName">
+    <button @click="changeName(newName)">Change Name</button>
+</template>
 
 <style scoped></style>
