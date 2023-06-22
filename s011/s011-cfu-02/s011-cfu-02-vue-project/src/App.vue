c88249d315785cc96d89253a5b5521dd79c18b21
nicodalessandro11 <nicodalessandro11@gmail.com>
2023-06-22 16:17:57 +0200

    cfu s011 finished

---
+++ s011/s011-cfu-02/s011-cfu-02-vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>CFU02 - Week #11</h1>
+    <h2 v-text="store.count"></h2>
+    <button @click="store.increment">Add one</button>
+    <button @click="store.decrement">Decrease One</button>
+    <button @click="store.reset">Reset</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from './store.js'
+
+const store = useCounterStore();
+
+</script>
+
+<style scoped></style>
